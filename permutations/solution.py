cb63cf92fbffcf05beb8a643b4bd8f66ff2405d9
SP321 <p.sandeep321@yahoo.com>
2023-08-02 22:12:49 +0530

    Problems solved on 2023-08-02

--- permutations/solution.py
+++ permutations/solution.py
@@ -1,15 +1,3 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        def dfs(seq):
-            if len(seq) == len(nums):
-                ans.append(list(seq))
-                return
-            for n in nums:
-                if n in seq:
-                    continue
-                seq.append(n)
-                dfs(seq)
-                seq.pop()
-        dfs([])
-        return ans
+        return permutations(nums)
