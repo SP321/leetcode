616bafa341c0e54ec79f4646d8435412a373e04a
SP321 <p.sandeep321@yahoo.com>
2023-09-25 20:41:16 +0530

    Problems solved on 2023-09-25

--- find-the-difference/solution.cpp
+++ find-the-difference/solution.cpp
@@ -1,11 +1,11 @@
 class Solution {
+int i;
 public:
     char findTheDifference(string s, string t) {
-        char result = 0;
-        for (char c : s)
-            result ^= c;
-        for (char c : t)
-            result ^= c;
-        return result;
+        for (i=0;i<s.size();i++){
+            t[0] ^= t[i+1];
+            t[0] ^= s[i];
+        }
+        return t[0];
     }
 };
--- find-the-difference/solution.py
+++ find-the-difference/solution.py
@@ -1,6 +1,8 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         ans=0
-        for i in s+t:
+        for i in s:
+            ans^=ord(i)
+        for i in t:
             ans^=ord(i)
         return chr(ans)
