0082ddc4f2022309f7a375109c9c5cba69cab856
SP321 <p.sandeep321@yahoo.com>
2023-10-05 20:02:52 +0530

    Problems solved on 2023-10-05

--- majority-element-ii/solution.py
+++ majority-element-ii/solution.py
@@ -1,11 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        c=defaultdict(int)
-        for i in nums:
-            c[i]+=1
-        ans=[]
-        n=len(nums)
-        for i in c:
-            if c[i]>(n//3):
-                ans.append(i)
-        return ans
+        c=Counter(nums)
+        return [i for i in c if c[i]>len(nums)//3]
