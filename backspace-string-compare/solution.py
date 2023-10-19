11958d50be6d59640536d146e52e6fce46663862
SP321 <p.sandeep321@yahoo.com>
2023-10-19 21:21:54 +0530

    Problems solved on 2023-10-19

---
+++ backspace-string-compare/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        st1,st2=[],[]
+        for i in s:
+            if i =='#':
+                if st1:
+                 st1.pop()
+            else:
+                st1.append(i)
+        for i in t:
+            if i =='#':
+                if st2:
+                    st2.pop()
+            else:
+                st2.append(i)
+        return st1==st2
