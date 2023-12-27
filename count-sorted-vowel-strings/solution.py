28aafc5c3d950d7c822519a54e658d2190bc24b1
SP321 <p.sandeep321@yahoo.com>
2023-12-27 22:47:07 +0530

    Problems solved on 2023-12-27

--- count-sorted-vowel-strings/solution.py
+++ count-sorted-vowel-strings/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        @cache
-        def dfs(i,c):
-            if c==0:
-                return 0
-            if i==n:
-                return 1
-            ans=0
-            for j in range(c+1):
-                ans+=dfs(i+1,j)
-            return ans
-        return dfs(0,5)
+        m=5
+        dp = [[0] *(m+1) for _ in range(n + 1)]
+        for j in range(m):
+            dp[n][j] = 1
+
+        for i in range(n)[::-1]:
+            for j in range(m):
+                ans = 0
+                for k in range(j + 1):
+                    ans += dp[i + 1][k]
+                dp[i][j] = ans
+        return dp[0][m-1]
+
