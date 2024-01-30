59dd95dfae68b19667095e238919cb91223efd30
SP321 <p.sandeep321@yahoo.com>
2024-01-30 21:45:30 +0530

    Problems solved on 2024-01-30

--- evaluate-reverse-polish-notation/solution.py
+++ evaluate-reverse-polish-notation/solution.py
@@ -1,17 +1,19 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        x=[]
-        for i in tokens:
-            if i == "+":
-                x.append(x.pop()+x.pop())
-            elif i == "-":
-                a=x.pop()
-                x.append(x.pop()-a)
-            elif i == "*":
-                x.append(x.pop()*x.pop())
-            elif i == "/":
-                a=x.pop()
-                x.append(int(x.pop()/a))
+        s=[]
+        for x in tokens:
+            if x not in "+-*/":
+                s.append(int(x))
             else:
-                x.append(int(i))
-        return x[-1]
+                b=s.pop()
+                a=s.pop()
+                if x=='+':
+                    s.append(a+b)
+                elif x=='-':
+                    s.append(a-b)
+                elif x=='*':
+                    s.append(a*b)
+                elif x=='/':
+                    val=a/b
+                    s.append(math.floor(val) if val>0 else math.ceil(val))
+        return s.pop()
