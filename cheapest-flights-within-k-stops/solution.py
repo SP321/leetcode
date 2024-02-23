c26cd579048d8adcd9b840d5c8f9f924ff0e0712
SP321 <p.sandeep321@yahoo.com>
2024-02-23 22:04:03 +0530

    Problems solved on 2024-02-23

--- cheapest-flights-within-k-stops/solution.py
+++ cheapest-flights-within-k-stops/solution.py
@@ -1,11 +1,24 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        prices = [inf] * n
-        prices[src] = 0
+        g=defaultdict(list)
+        for u, v, p in flights:
+            g[u].append((v, p))
+        
+        q = deque()
+        q.append((0,src))
 
-        for i in range(k + 1):
-            next_prices = prices.copy()
-            for s, d, p in flights:
-                next_prices[d] = min(next_prices[d] , prices[s] + p)
-            prices = next_prices
-        return -1 if prices[dst] == inf else prices[dst]
+        dists = [float('inf') for _ in range(n)]
+        dists[src] = 0
+
+        for _ in range(k+1):
+            if not q:
+                break
+            q2=[]
+            for dist,u in q:
+                for v, w in g[u]:
+                    if dists[v] > dist + w:
+                        dists[v] = dist + w
+                        q2.append((dists[v], v))
+            q=q2
+        
+        return -1 if dists[dst] == float('inf') else dists[dst]
