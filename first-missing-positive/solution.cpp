065bd9574e84aa37f64c5443449b95e8d2c7ac6a
SP321 <p.sandeep321@yahoo.com>
2024-03-26 22:09:25 +0530

    Problems solved on 2024-03-26

--- first-missing-positive/solution.cpp
+++ first-missing-positive/solution.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n=nums.size();
-        vector<int>c(n+2);
         for(int i=0;i<n;i++){
-            if(nums[i]>0 &&nums[i]<=n)
-                c[nums[i]]=1;
+            if(nums[i]<=0 or nums[i]>n)
+                nums[i]=n+1;
         }
-        for(int i=1;i<=n+1;i++)
-            if(!c[i])
-                return i;
-        return -1;
+        for(int i=0;i<n;i++){
+            int cur=abs(nums[i]);
+            if(cur==n+1)
+                continue;
+            if(nums[cur-1]>0)
+                nums[cur-1]=-nums[cur-1];
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>0)
+                return i+1;
+        }
+        return n+1;
     }
 };
--- first-missing-positive/solution.py
+++ first-missing-positive/solution.py
@@ -1,10 +1,15 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        n=len(nums)
-        c=[0]*(n+2)
-        for i in nums:
-            if i>0 and i<=n:
-                c[i]=1
-        for i in range(1,n+2):
-            if c[i]==0:
-                return i
+        n = len(nums)
+		
+        for i in range(n):
+            if nums[i] <= 0 or nums[i] > n:
+                nums[i] = n + 1
+        for i in range(n):
+            if abs(nums[i]) > n:
+                continue
+            nums[abs(nums[i]) - 1] = -abs(nums[abs(nums[i]) - 1])
+        for i in range(n):
+            if nums[i] > 0:
+                return i + 1
+        return n + 1
