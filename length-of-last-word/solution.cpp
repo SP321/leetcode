34f525d6779b5dc87b9ae30bba8b0dfafe20965a
SP321 <p.sandeep321@yahoo.com>
2024-04-01 21:46:35 +0530

    Problems solved on 2024-04-01

--- length-of-last-word/solution.cpp
+++ length-of-last-word/solution.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        int ans=0;
         int i=s.size()-1;
-        while(i>=0 && s[i]==' ')i--;
-        int j=i;
-        while(j>=0 && s[j]!=' ')j--;
-        return i-j;
+        while(s[i]==' ')
+            i-=1;
+        while(i>=0 and s[i]!=' '){
+            i-=1;
+            ans+=1;
+        }
+        return ans;
     }
 };
--- length-of-last-word/solution.py
+++ length-of-last-word/solution.py
@@ -1,3 +1,10 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        return len(s.strip().split(' ')[-1])
+        ans=0
+        i=len(s)-1
+        while s[i]==' ':
+            i-=1
+        while i>=0 and s[i]!=' ':
+            ans+=1
+            i-=1
+        return ans
