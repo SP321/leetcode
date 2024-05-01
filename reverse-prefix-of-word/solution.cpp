ff0852d882f206ddce2e7ec5bc5d42a437baf86b
SP321 <p.sandeep321@yahoo.com>
2024-05-01 21:50:37 +0530

    Problems solved on 2024-05-01

--- reverse-prefix-of-word/solution.cpp
+++ reverse-prefix-of-word/solution.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        for(int i=0;i<word.size();i++)
-            if(ch==word[i]){
-                reverse(word.begin(),word.begin()+i+1);
-                break;
-            }
+        reverse(begin(word), begin(word) + word.find(ch) + 1);
         return word;
     }
 };
--- reverse-prefix-of-word/solution.py
+++ reverse-prefix-of-word/solution.py
@@ -1,6 +1,8 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        for i in range(len(word)):
-            if word[i]==ch:
-                return word[:i+1][::-1]+word[i+1:]
+        a=[]
+        for i,x in enumerate(word):
+            a.append(x)
+            if x==ch:
+                return ''.join(a[::-1])+word[i+1:]
         return word
