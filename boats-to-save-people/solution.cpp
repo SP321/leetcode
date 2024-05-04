bc379e6d291ab3a6406b585110328bb16433ca00
SP321 <p.sandeep321@yahoo.com>
2024-05-04 22:17:47 +0530

    Problems solved on 2024-05-04

--- boats-to-save-people/solution.cpp
+++ boats-to-save-people/solution.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        vector<int>count(limit+1,0);
-        int ans=0;
-        for(int &x:people)
-            count[x]++;
-        int pairs,j;
-        for(int i=1;i<count.size();i++){
-            j=limit-i;
-            while(count[i]&&j>i){
-                pairs=min(count[i],count[j]);
-                ans+=pairs;
-                count[j]-=pairs;
-                count[i]-=pairs;
-                j--;
+        sort(people.begin(), people.end());
+        int i = 0, j = people.size() - 1;
+        int ans = 0;
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
             }
-            if(i*2<=limit)
-                ans+=count[i]/2+count[i]%2;
-            else
-                ans+=count[i];
+            j--;
+            ans++; 
         }
         return ans;
     }
