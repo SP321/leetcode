f3c0b2851521c42f491c826061d7311ea23e3901
SP321 <p.sandeep321@yahoo.com>
2024-07-27 20:12:38 +0530

    Problems solved on 2024-07-27

--- minimum-cost-to-convert-string-i/solution.py
+++ minimum-cost-to-convert-string-i/solution.py
@@ -1,19 +1,16 @@
 class Solution:
     def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
-        g=defaultdict(lambda :float('inf'))
+        g=defaultdict(lambda :inf)
         for u,v,w in zip(original,changed,cost):
             g[u,v]=min(g[u,v],w)
-        for k in range(ord('a'),ord('z')+1):
-            for i in range(ord('a'),ord('z')+1):
-                for j in range(ord('a'),ord('z')+1):
-                    ch_i=chr(i)
-                    ch_j=chr(j)
-                    ch_k=chr(k)
-                    g[ch_i,ch_j]=min(g[ch_i,ch_j],g[ch_i,ch_k]+g[ch_k,ch_j])
+        for k in ascii_lowercase:
+            for i in ascii_lowercase:
+                for j in ascii_lowercase:
+                    g[i,j]=min(g[i,j],g[i,k]+g[k,j])
         ans=0
         for a,b in zip(source,target):
             if a!=b:
-                if g[a,b]!=float("inf"):
+                if g[a,b]!=inf:
                     ans+=g[a,b]
                 else:
                     return -1
