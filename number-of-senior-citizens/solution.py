dd39effff3b0e1f43662cf6f0745c06493930e5e
SP321 <p.sandeep321@yahoo.com>
2024-08-01 22:48:24 +0530

    Problems solved on 2024-08-01

--- number-of-senior-citizens/solution.py
+++ number-of-senior-citizens/solution.py
@@ -1,5 +1,4 @@
 class Solution:
     def countSeniors(self, details: List[str]) -> int:
-        x=[int(i[-4:-2]) for i in details]
-        return sum([1 for i in x if i>60])
+        return sum([1 for x in details if int(x[-4:-2])>60])
         
