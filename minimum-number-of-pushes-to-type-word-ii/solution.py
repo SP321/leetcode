57df07346a3508b42617b3f3c8f4458e5caa02a0
SP321 <p.sandeep321@yahoo.com>
2024-08-06 20:40:46 +0530

    Problems solved on 2024-08-06

--- minimum-number-of-pushes-to-type-word-ii/solution.py
+++ minimum-number-of-pushes-to-type-word-ii/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
+        c=Counter(word)
+        a=list(c.values())
+        a.sort(reverse=True)
         ans=0
-        c=0
-        for ch,x in Counter(word).most_common():
-            ans+=x*((c//8)+1)
-            c+=1
+        for i in range(len(a)):
+            ans+=a[i]*((i//8) +1)
         return ans
-            
