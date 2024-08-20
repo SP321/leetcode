8f49321141f639f875c912b6d5d192f5648f7dd0
SP321 <p.sandeep321@yahoo.com>
2024-08-20 20:55:57 +0530

    Problems solved on 2024-08-20

--- stone-game-ii/solution.py
+++ stone-game-ii/solution.py
@@ -1,14 +1,12 @@
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
         n=len(piles)
-        dp={}
+        @cache
         def dfs(alice,i,m):
-            if((alice,i,m)) in dp:
-                return dp[(alice,i,m)]
             if i==n:
                 return 0
             ma=0
-            mi=1e9
+            mi=inf
             stones=0
             for x in range(1,2*m+1):
                 j=i+x-1
@@ -18,8 +16,7 @@ class Solution:
                 move=dfs(not alice,j+1,max(m,x))
                 ma=max(ma,stones+move)
                 mi=min(mi,move)
-            dp[(alice,i,m)]=ma if alice else mi
-            return dp[(alice,i,m)]
+            return ma if alice else mi
         return dfs(True,0,1)
 
 
