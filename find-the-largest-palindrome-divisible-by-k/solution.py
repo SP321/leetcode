28a469b4fb8b36a0f6d26dce3bcdfd508d40209a
SP321 <p.sandeep321@yahoo.com>
2024-08-21 22:36:38 +0530

    Problems solved on 2024-08-21

--- find-the-largest-palindrome-divisible-by-k/solution.py
+++ find-the-largest-palindrome-divisible-by-k/solution.py
@@ -1,24 +1,30 @@
 class Solution:
-    def largestPalindrome(self, N: int, K: int) -> str:
-        pow10 = [1] * N
-        for i in range(1, N):
-            pow10[i] = pow10[i-1] * 10 % K
+    def largestPalindrome(self, n: int, k: int) -> str:
+        ans = [0 for _ in range(n)]
         
-        ans = [9] * N
-
+        p = [1]*(n + 1)
+        
+        for i in range(1, n + 1):
+            p[i] = (p[i - 1]*10) % k
+        
         @cache
         def dp(i, r):
-            if i == (N + 1) // 2:
+            if i == (n // 2 + (n&1)):
                 return r == 0
             
             for d in range(9, -1, -1):
-                ans[i] = ans[N-1-i] = d
-                coeff = sum(pow10[j] for j in {i, N-1-i})
-                r2 = (r + coeff * d) % K
-                if dp(i + 1, r2):
+                ans[i] = chr(d + ord('0'))
+                ans[n - i - 1] = chr(d + ord('0'))
+                
+                if i != n - i - 1:
+                    nr = (r + d*p[i] + d*p[n - i - 1]) % k
+                else:
+                    nr = (r + d*p[i]) % k
+                
+                if dp(i + 1, nr):
                     return True
-            
+                
             return False
-
+                
         dp(0, 0)
-        return "".join(map(str, ans))
+        return ''.join(ans)
