af50cf7433a4eec09a35f6c937743e221909f232
SP321 <p.sandeep321@yahoo.com>
2024-09-21 22:47:10 +0530

    Problems solved on 2024-09-21

--- maximum-multiplication-score/solution.py
+++ maximum-multiplication-score/solution.py
@@ -1,13 +1,14 @@
 class Solution:
     def maxScore(self, a: List[int], b: List[int]) -> int:
-        n=len(b)
-        @cache
-        def dp(i,j):
-            if j==4:
-                return 0
-            if i==n:
-                return -inf
-            ans=dp(i+1,j)
-            ans=max(ans,dp(i+1,j+1)+b[i]*a[j])
-            return ans
-        return dp(0,0)
+        a0,a1,a2,a3 = a
+        w,x,y,z = -inf,-inf,-inf,-inf
+        for c in b:
+            if a3*c+y > z:
+                z = a3*c+y
+            if a2*c+x > y:
+                y = a2*c+x
+            if a1*c+w > x:
+                x = a1*c+w
+            if a0*c > w:
+                w = a0*c
+        return z
