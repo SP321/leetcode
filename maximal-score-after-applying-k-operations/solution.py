d98a8585b718da80432883db80ed7efac678a3aa
SP321 <p.sandeep321@yahoo.com>
2024-10-14 20:14:55 +0530

    Problems solved on 2024-10-14

--- maximal-score-after-applying-k-operations/solution.py
+++ maximal-score-after-applying-k-operations/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def maxKelements(self, nums: List[int], k: int) -> int:
-        h=[-x for x in nums]
-        heapify(h)
+        h=[]
+        for x in nums:
+            heappush(h,-x)
         ans=0
         for _ in range(k):
-            x=-heappop(h)
-            ans+=x
-            heappush(h,- math.ceil(x/3))
+            cur=-heappop(h)
+            ans+=cur
+            heappush(h,-math.ceil(cur/3))
         return ans
