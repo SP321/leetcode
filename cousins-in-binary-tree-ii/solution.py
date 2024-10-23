40eb8d8146207f29d38314952c6fe1e5d377147a
SP321 <p.sandeep321@yahoo.com>
2024-10-23 20:57:17 +0530

    Problems solved on 2024-10-23

--- cousins-in-binary-tree-ii/solution.py
+++ cousins-in-binary-tree-ii/solution.py
@@ -6,32 +6,20 @@
 #         self.right = right
 class Solution:
     def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        depth_sum={}
-        depth_map={}
-        sibling={}
-        def dfs(i,x):
-            if x not in depth_sum:
-                depth_sum[x]=0
-                depth_map[x]=[]
-            depth_sum[x]+=i.val
-            depth_map[x].append(i)
-            if i.left:
-                sibling[i.left]=i.right
-                dfs(i.left,x+1)
-            if i.right:
-                sibling[i.right]=i.left
-                dfs(i.right,x+1)
-        dfs(root,0)
-        sibling[root]=False
-        for i in depth_sum:
-            newval={}
-            for cur in depth_map[i]:
-                val=depth_sum[i]-cur.val
-                if sibling[cur]:
-                    val-=sibling[cur].val
-                newval[cur]=val
-            for cur in depth_map[i]:
-                cur.val=newval[cur]
+        q=[(root,0)]
+        while q:
+            q2=[]
+            total=0
+            d=Counter()
+            for node,par in q:
+                total+=node.val
+                d[par]+=node.val
+            for node,par in q:
+                if node.left:
+                    q2.append( (node.left,node) )
+                if node.right:
+                    q2.append( (node.right,node) )
+                node.val=total-d[par]
+            q=q2
         return root
             
-
