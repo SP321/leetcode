17bbcb5d89b5900faa777c9bf1cbccf2bbede7bc
SP321 <p.sandeep321@yahoo.com>
2024-11-04 21:40:32 +0530

    Problems solved on 2024-11-04

--- string-compression-iii/solution.py
+++ string-compression-iii/solution.py
@@ -2,10 +2,10 @@ class Solution:
     def compressedString(self, word: str) -> str:
         ans=[]
         for x in groupby(word):
-            y=len(list(x[1]))
-            while y!=0:
-                z=min(y,9)
-                ans.append(str(z))
-                ans.append(x[0])
-                y-=z
+            x,y=x[0],len(list(x[1]))
+            z=y//9
+            if z!=0:
+                ans.append(f"9{x}"*z)
+            if y%9!=0:
+                ans.append(f"{y%9}{x}")
         return ''.join(ans)
