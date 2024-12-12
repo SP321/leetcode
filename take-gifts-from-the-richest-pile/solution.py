2c8fc71df082f328c1dbce54fdaa86c15d6f04f4
SP321 <p.sandeep321@yahoo.com>
2024-12-12 20:04:35 +0530

    Problems solved on 2024-12-12

--- take-gifts-from-the-richest-pile/solution.py
+++ take-gifts-from-the-richest-pile/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        h=[-x for x in gifts]
-        heapify(h)
+        h=[]
+        for x in gifts:
+            heappush(h,-x)
         for _ in range(k):
-            x=heappop(h)
-            heappush(h,-int(sqrt(-x)))
+            cur=-heappop(h)
+            heappush(h,-(int(sqrt(cur))))
         return -sum(h)
